ecddc1dc9b18fd50a30442642b294b4d01e8274a
Vinícius Cossio de Oliveira <vinicossio@gmail.com>
2025-09-10 11:51:47 -0300

    client update connected to API. Missing css and masks auto completed

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 import Home from './Pages/Home/Home';
 import CreateService from './Pages/Home/CreateService/CreateService';
 import EditService from './Pages/Home/EditService/EditService';
+import EditUser from './Pages/User/EditUser/EditUser';
+import User from './Pages/User/User';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -15,6 +17,8 @@ root.render(
       <Route path = "/home" element= {<Home/>}/>
       <Route path="/create-service" element={<CreateService />} />
       <Route path = "/edit-service/:serviceId" element = {<EditService/>}/>
+      <Route path = "/users" element = {<User/>}/>
+      <Route path = "/edit-client/:clientId" element = {<EditUser/>}/>
     </Routes>
   </BrowserRouter>
 );
